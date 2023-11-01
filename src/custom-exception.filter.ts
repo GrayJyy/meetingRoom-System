84f5373ffc85936e414e094639f4e25ef2681064
gray_jiang <Gray_Jy@yeah.net>
2023-11-01 16:45:47 +0800

    fix: 解决字段校验错误信息不完整问题

--- src/custom-exception.filter.ts
+++ src/custom-exception.filter.ts
@@ -11,11 +11,14 @@ export class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
     response.statusCode = exception.getStatus();
+
+    const res = exception.getResponse() as { message: string[] };
+
     response
       .json({
         code: exception.getStatus(),
         message: 'fail',
-        data: exception.message,
+        data: res?.message?.join ? res?.message?.join(',') : exception.message,
       })
       .end();
   }
